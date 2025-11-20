17fb7c748a6e821a816592b3fd837d61e43d981f
Raj Khare <rajkhare78946@gmail.com>
2025-11-20 22:10:27 +0530

    Fixed API issues

--- src/vite-env.d.js
+++ src/vite-env.d.js
@@ -1,4 +1,4 @@
 
 // Vite client types are automatically available in JavaScript projects
 // This file can remain empty for JavaScript projects
-VITE_API_URL = "https://team-tracker-x3tl.onrender.com"
+VITE_API_URL = "https://team-tracker-x3tl.onrender.com/api"
